97b6d68fc90c42ed5b78b29db7332b3540d1718c
Langusterno <147515138+Langusterno@users.noreply.github.com>
2023-10-10 15:46:36 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -21,9 +21,6 @@ forever(function () {
             . 3 3 3 3 3 . 3 3 3 3 . 3 3 3 3 
             . . . . . . . . . . . . . . . . 
             `, SpriteKind.Food)
-        Seconds.setPosition(86, 51)
-        Seconds.sayText(Second)
-        pause(1000)
     }
     Second = 0
 })
